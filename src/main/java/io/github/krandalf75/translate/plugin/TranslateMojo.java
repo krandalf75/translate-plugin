0f9cc9382082869e0b32d4909ad0f1fda599f951
Krandalf75 <krandalf75@gmail.com>
2024-01-12 17:55:19 +0100

    Changes to use ChatCompletionAPI and "gpt-3.5-turbo". Update com.theokanning.openai-gpt3-java dependency to 0.18.2. Change version to 1.1

--- src/main/java/io/github/krandalf75/translate/plugin/TranslateMojo.java
+++ src/main/java/io/github/krandalf75/translate/plugin/TranslateMojo.java
@@ -1,8 +1,10 @@
 package io.github.krandalf75.translate.plugin;
 
-import com.theokanning.openai.completion.CompletionChoice;
-import com.theokanning.openai.completion.CompletionRequest;
-import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,6 +12,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import nu.studer.java.util.OrderedProperties;
@@ -136,25 +140,29 @@ public class TranslateMojo extends AbstractMojo {
             return value;
         }
 
-        String prompt = "On the next line, I will pass a <key>=<value> string. Translate the value from locale '" + sourceLocale + "' to locale '" + locale + "'. Answer only with format <key>=<value>. Ensure that the translations are accurate and maintain the <key>=<value> structure.\n" + key + "=" + value;
+        String prompt = "Translate the following text fragment from the properties file with clang \"" + sourceLocale + " to clang \"" + locale + "\". " +
+            "Answer only with the translation. The fragment is:\n" +
+            key + "=" + value;
         getLog().info("Prompt:" + prompt);
 
-        CompletionRequest request = CompletionRequest.builder()
-                .model("text-davinci-003")
-                .prompt(prompt)
-                .echo(false)
-                .user("hhh")
-                .maxTokens(1024)
+        final List<ChatMessage> messages = new ArrayList<>();
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.USER.value(), prompt);
+        messages.add(systemMessage);
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-3.5-turbo")
+                .messages(messages)
                 .n(1)
+                .maxTokens(50)
+                .logitBias(new HashMap<>())
                 .build();
 
-        CompletionResult result = service.createCompletion(request);
-        getLog().info("model:" + result.getModel());
+        ChatCompletionResult result = service.createChatCompletion(chatCompletionRequest);
 
-        List<CompletionChoice> choices = result.getChoices();
+        List<ChatCompletionChoice> choices = result.getChoices();
 
         if (choices != null && !choices.isEmpty()) {
-            String text = choices.get(0).getText();
+            String text = choices.get(0).getMessage().getContent();
             getLog().info("translation:" + text);
             text = text.substring(text.indexOf("=") + 1).trim();
             getLog().info("text:" + text);
